64ca2c41a051de79b346d07892585ec114bdce02
Zeynab Khani <41279900+ZeynabKhani@users.noreply.github.com>
2025-02-03 09:10:36 +0330

    got new keyblocks for testnet

--- scripts/testnet/getKeyBlocks.ts
+++ scripts/testnet/getKeyBlocks.ts
@@ -1,7 +1,6 @@
 import { UserFriendlyValidator } from '@oraichain/tonbridge-contracts-sdk/build/TonbridgeValidator.types';
 import TonRocks, { ParsedBlock, pubkeyHexToEd25519DER, ValidatorSignature } from '@oraichain/tonbridge-utils';
-import { Cell as TonRocksCell } from '@oraichain/tonbridge-utils/build/types/Cell';
-import { Cell, Address, loadTransaction } from '@ton/core';
+import { Cell } from '@ton/core';
 import { sha256 } from '@ton/crypto';
 import assert from 'assert';
 import crypto from 'crypto';
@@ -10,8 +9,6 @@ import { LiteClient, LiteEngine, LiteRoundRobinEngine, LiteSingleEngine } from '
 import { Functions, liteServer_BlockData, tonNode_blockIdExt } from 'ton-lite-client/dist/schema';
 import TonWeb from 'tonweb';
 import * as fs from 'fs';
-import { sleep } from '@ton/blueprint';
-import { loadBlockExtra } from '@oraichain/tonbridge-utils/build/blockchain/BlockParser';
 
 export function intToIP(int: number) {
     var part1 = int & 255;
@@ -31,115 +28,121 @@ export async function parseBlock(block: liteServer_BlockData): Promise<ParsedBlo
     return parsedBlock;
 }
 
-async function verifyMasterchainBlock(liteClient: LiteClient, blockId: tonNode_blockIdExt, validatorSet: any) {
-    const block = await liteClient.engine.query(Functions.liteServer_getBlock, {
-        kind: 'liteServer.getBlock',
-        id: blockId,
-    });
-    const parsedBlock = await parseBlock(block);
-
+async function verifyMasterchainBlock(
+    liteClient: LiteClient,
+    blockId: tonNode_blockIdExt,
+    validatorSet: any,
+    keyBlockNumber: number,
+) {
     // Verifying the block header which is an exotic merkle proof cell with pruned branches
     const blockHeader = await liteClient.getBlockHeader(blockId);
     const blockHash = Cell.fromBoc(blockHeader.headerProof)[0].refs[0].hash(0);
     assert(blockHash.toString('hex') === blockHeader.id.rootHash.toString('hex'));
-    console.log('masterchain block:', blockId.seqno);
+    console.log('Verified block header of masterchain block: ', blockId.seqno);
 
     const tonweb = new TonWeb(
         new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {
             apiKey: process.env.apiKey,
         }),
     );
+
+    let signatures: ValidatorSignature[];
     try {
         const valSignatures = (await tonweb.provider.send('getMasterchainBlockSignatures', {
             seqno: blockId.seqno,
         })) as any;
-        const signatures = valSignatures.signatures as ValidatorSignature[];
-        // for (const signature of signatures) {
-        //     console.log('signature:', signature.node_id_short);
-        // }
-        const signaturesData = {
-            signatures: signatures,
-        };
-        let existingData = {};
-        if (fs.existsSync('tests/fastnet/keyblock1.json')) {
-            existingData = JSON.parse(fs.readFileSync('tests/fastnet/keyblock1.json', 'utf8'));
-        }
-        const mergedData = { ...existingData, ...signaturesData };
-        fs.writeFileSync('tests/fastnet/keyblock1.json', JSON.stringify(mergedData, null, 2));
-        console.log('Signatures written to tests/fastnet/keyblock1.json');
-
-        let friendlyValidators: UserFriendlyValidator[] = [];
-        if (parsedBlock.extra?.custom?.config?.config?.map?.get('24') === undefined) {
-            const prevKeyBlock = await liteClient.getFullBlock(parsedBlock.info.prev_key_block_seqno);
-            const matchingShard = prevKeyBlock.shards.find(
-                (shard) => shard.seqno === parsedBlock.info.prev_key_block_seqno,
-            );
-            if (!matchingShard) {
-                throw new Error(`No shard found with seqno ${parsedBlock.info.prev_key_block_seqno}`);
-            }
-            const prevBlock = await liteClient.engine.query(Functions.liteServer_getBlock, {
-                kind: 'liteServer.getBlock',
-                id: {
-                    kind: 'tonNode.blockIdExt',
-                    ...matchingShard,
-                },
-            });
-            const prevParsedBlock = await parseBlock(prevBlock);
-
-            // Get previous validator set from config
-            const prevValidatorSet = prevParsedBlock.extra?.custom?.config?.config?.map?.get('22');
-            if (!prevValidatorSet) {
-                throw new Error('Could not find previous validator set for key block verification');
-            }
-            validatorSet = prevValidatorSet;
-        }
-
-        for (const entry of validatorSet.cur_validators.list.map.entries()) {
-            // magic number prefix for a node id of a validator
-            const nodeIdPrefix = Buffer.from([0xc6, 0xb4, 0x13, 0x48]);
-            const pubkey = entry[1].public_key.pubkey;
-            // we need nodeId because the validator signatures returned from Toncenter only have nodeIds
-            const nodeId = await sha256(Buffer.concat([nodeIdPrefix, pubkey]));
-            friendlyValidators.push({
-                ...entry[1],
-                node_id: nodeId.toString('base64'),
-                weight: +entry[1].weight.toString(),
-                pubkey,
-            });
-        }
-
-        const sumLargestTotalWeights = friendlyValidators
-            .sort((a: any, b: any) => b.weight - a.weight)
-            .slice(0, 100)
-            .map((val: any) => val.weight)
-            .reduce((prev: any, cur: any) => prev + cur);
-        const message = Buffer.concat([
-            // magic prefix of message signing
-            Buffer.from([0x70, 0x6e, 0x0b, 0xc5]),
-            blockHash,
-            blockHeader.id.fileHash,
-        ]);
-        let totalWeight = 0;
-        for (const item of signatures) {
-            const validator = friendlyValidators.find((val: any) => val.node_id === item.node_id_short);
-            if (!validator) continue;
-            const key = pubkeyHexToEd25519DER(validator.pubkey);
-            const verifyKey = crypto.createPublicKey({
-                format: 'der',
-                type: 'spki',
-                key,
-            });
-            const result = crypto.verify(null, message, verifyKey, Buffer.from(item.signature, 'base64'));
-            assert(result === true);
-            totalWeight += validator.weight;
-        }
-        assert(totalWeight > 0);
-        assert(totalWeight * 3 > sumLargestTotalWeights * 2);
-        console.log('Masterchain block is verified successfully!');
+        signatures = valSignatures.signatures as ValidatorSignature[];
     } catch (error) {
         console.error('Error fetching block signatures:', error);
         throw error;
     }
+
+    const block = await liteClient.engine.query(Functions.liteServer_getBlock, {
+        kind: 'liteServer.getBlock',
+        id: blockId,
+    });
+    const parsedBlock = await parseBlock(block);
+    if (parsedBlock.extra?.custom?.config?.config?.map?.get('24') === undefined) {
+        // If the keyblock does not have a next validator set, we need to get the previous keyblock to get its current validator set to check with signatures
+        console.log('Key block does not have a next validator set, getting previous keyblock...');
+        const prevKeyBlock = await liteClient.getFullBlock(parsedBlock.info.prev_key_block_seqno);
+        const matchingShard = prevKeyBlock.shards.find(
+            (shard) => shard.seqno === parsedBlock.info.prev_key_block_seqno,
+        );
+        if (!matchingShard) {
+            throw new Error(`No shard found with seqno ${parsedBlock.info.prev_key_block_seqno}`);
+        }
+        const prevBlock = await liteClient.engine.query(Functions.liteServer_getBlock, {
+            kind: 'liteServer.getBlock',
+            id: {
+                kind: 'tonNode.blockIdExt',
+                ...matchingShard,
+            },
+        });
+        const prevParsedBlock = await parseBlock(prevBlock);
+
+        const prevValidatorSet = prevParsedBlock.extra?.custom?.config?.config?.map?.get('22');
+        if (!prevValidatorSet) {
+            throw new Error('Could not find previous validator set for key block verification');
+        }
+        validatorSet = prevValidatorSet;
+    }
+
+    let friendlyValidators: UserFriendlyValidator[] = [];
+    for (const entry of validatorSet.cur_validators.list.map.entries()) {
+        // magic number prefix for a node id of a validator
+        const nodeIdPrefix = Buffer.from([0xc6, 0xb4, 0x13, 0x48]);
+        const pubkey = entry[1].public_key.pubkey;
+        // we need nodeId because the validator signatures returned from Toncenter only have nodeIds
+        const nodeId = await sha256(Buffer.concat([nodeIdPrefix, pubkey]));
+        friendlyValidators.push({
+            ...entry[1],
+            node_id: nodeId.toString('base64'),
+            weight: +entry[1].weight.toString(),
+            pubkey,
+        });
+    }
+
+    const sumLargestTotalWeights = friendlyValidators
+        .sort((a: any, b: any) => b.weight - a.weight)
+        .slice(0, 100)
+        .map((val: any) => val.weight)
+        .reduce((prev: any, cur: any) => prev + cur);
+    const message = Buffer.concat([
+        // magic prefix of message signing
+        Buffer.from([0x70, 0x6e, 0x0b, 0xc5]),
+        blockHash,
+        blockHeader.id.fileHash,
+    ]);
+    let totalWeight = 0;
+    for (const item of signatures) {
+        const validator = friendlyValidators.find((val: any) => val.node_id === item.node_id_short);
+        if (!validator) continue;
+        const key = pubkeyHexToEd25519DER(validator.pubkey);
+        const verifyKey = crypto.createPublicKey({
+            format: 'der',
+            type: 'spki',
+            key,
+        });
+        const result = crypto.verify(null, message, verifyKey, Buffer.from(item.signature, 'base64'));
+        assert(result === true);
+        totalWeight += validator.weight;
+    }
+    assert(totalWeight > 0);
+    assert(totalWeight * 3 > sumLargestTotalWeights * 2);
+    console.log('Masterchain block is verified successfully!');
+
+    // Now that signatures are verified, we can write them to the file
+    const signaturesData = {
+        signatures: signatures,
+    };
+    let existingData = {};
+    if (fs.existsSync('tests/testnet/keyblock' + keyBlockNumber + '.json')) {
+        existingData = JSON.parse(fs.readFileSync('tests/testnet/keyblock' + keyBlockNumber + '.json', 'utf8'));
+    }
+    const mergedData = { ...existingData, ...signaturesData };
+    fs.writeFileSync('tests/testnet/keyblock' + keyBlockNumber + '.json', JSON.stringify(mergedData, null, 2));
+    console.log('Signatures written to tests/testnet/keyblock' + keyBlockNumber + '.json');
 }
 
 (async () => {
@@ -167,11 +170,9 @@ async function verifyMasterchainBlock(liteClient: LiteClient, blockId: tonNode_b
     let blockInfo = master.last;
     let parsedBlock: ParsedBlock;
     let validatorSet;
-    let nextValidatorSet;
-    let prevValidatorSet;
-    let numberOfKeyBlocks = 0;
+    let keyBlockNumber = 1;
+    let counter = 4 - keyBlockNumber;
     while (true) {
-        // get block
         const block = await engine.query(Functions.liteServer_getBlock, {
             kind: 'liteServer.getBlock',
             id: blockInfo,
@@ -196,15 +197,13 @@ async function verifyMasterchainBlock(liteClient: LiteClient, blockId: tonNode_b
             if (validatorSet === undefined) {
                 console.log('No validator set found in config (key 22)');
             } else {
-                console.log('parsed key block seqno:', parsedBlock.info.seq_no);
-                // console.log('parsed block validator set:', validatorSet);
+                console.log('Found validator set in config (key 22) for key block seqno:', parsedBlock.info.seq_no);
             }
         } catch (error) {
             console.error('Error accessing block config:', error);
         }
 
-        numberOfKeyBlocks++;
-        if (numberOfKeyBlocks > 0) {
+        if (counter === 0) {
             const testData = {
                 block: {
                     kind: block.kind,
@@ -212,10 +211,11 @@ async function verifyMasterchainBlock(liteClient: LiteClient, blockId: tonNode_b
                     data: block.data,
                 },
             };
-            fs.writeFileSync('tests/block0.json', JSON.stringify(testData, null, 2));
-            console.log('Block data written to tests/block0.json');
+            fs.writeFileSync('tests/testnet/keyblock' + keyBlockNumber + '.json', JSON.stringify(testData, null, 2));
+            console.log('Block data written to tests/testnet/keyblock' + keyBlockNumber + '.json');
             break;
         }
+        counter--;
         const keyBlockInfo = await client.getFullBlock(parsedBlock.info.prev_key_block_seqno);
         const matchingShard = keyBlockInfo.shards.find(
             (shard) => shard.seqno === parsedBlock.info.prev_key_block_seqno,
@@ -238,14 +238,14 @@ async function verifyMasterchainBlock(liteClient: LiteClient, blockId: tonNode_b
         },
     };
     let existingData = {};
-    if (fs.existsSync('tests/block0.json')) {
-        existingData = JSON.parse(fs.readFileSync('tests/block0.json', 'utf8'));
+    if (fs.existsSync('tests/testnet/keyblock' + keyBlockNumber + '.json')) {
+        existingData = JSON.parse(fs.readFileSync('tests/testnet/keyblock' + keyBlockNumber + '.json', 'utf8'));
     }
     const mergedData = { ...existingData, ...testData };
-    fs.writeFileSync('tests/block0.json', JSON.stringify(mergedData, null, 2));
-    console.log('Block header written to tests/block0.json');
+    fs.writeFileSync('tests/testnet/keyblock' + keyBlockNumber + '.json', JSON.stringify(mergedData, null, 2));
+    console.log('Block header written to tests/testnet/keyblock' + keyBlockNumber + '.json');
 
-    await verifyMasterchainBlock(client, blockInfo, validatorSet);
+    await verifyMasterchainBlock(client, blockInfo, validatorSet, keyBlockNumber);
 
     engine.close();
 })();
